ef71150773c6a47baa44f1fe1807807206ba0679
Mario Torrez <62448948+Maritush@users.noreply.github.com>
2021-07-07 19:53:44 -0400

    Update pronostico_de_helada_UAC_Batallas.py

--- pronostico_de_helada_UAC_Batallas.py
+++ pronostico_de_helada_UAC_Batallas.py
@@ -39,14 +39,16 @@ db = firestore.Client(credentials=creds, project="custom-temple-312913") #Use Pr
 
 forcast_collection = db.collection('Forecasts')
 
-timezone_diff= timedelta(hours = 4)
-now = datetime.now() - timezone_diff
+
+now = datetime.now()
+if now.minute < 3:
+    now = now -timedelta(hours=1)
 doc = forcast_collection.document(now.strftime('%Y%m%d%H00'))  # specifies the '202103171800' document
 values = doc.get().to_dict()
 
 offset = timedelta(hours = 1)
 two_days_offset = timedelta(hours = 48) 
-before = datetime.now() - two_days_offset - timezone_diff
+before = datetime.now() - two_days_offset 
 measured_range = pd.date_range(start= before+offset,periods = 48,freq='h')
 forecasted_range = pd.date_range(start= now+offset,periods = 24,freq='h')
 measured = pd.DataFrame(values['measured'][::-1],index = pd.to_datetime(measured_range.strftime('%Y%m%d%H00')))
@@ -67,7 +69,14 @@ st.title('Sistema de pronostico y alerta temprana ante heladas')
 # st.plotly_chart(fig)
 
 
-
+std = np.array([0,0.1538073960085951, 0.182573145216934, 0.2684288896918868,
+       0.3564124460799613, 0.40493668007771894, 0.5208442654896113,
+       0.48466375049222965, 0.49739921107537455, 0.6086748652457432,
+       0.6790776796340291, 0.6646592778762843, 0.6444529749933195,
+       0.643997241049695, 0.6586570311345119, 0.6830488941790964,
+       0.7202596734098968, 0.7316954520595191, 0.7369931070178055,
+       0.7880383802271815, 0.8939606544494744, 1.0266284838943134,
+       1.1250693224470059, 1.2478295309199694, 1.3765725260136858])
 
 forecast_load_chart = go.Scatter(x=forecasted.index, y=forecasted[0],
                           mode='lines',              
@@ -86,12 +95,12 @@ measured_load_chart= go.Scatter(x=measured.index, y=measured[0],
 
 upper_bound_forecast_chart = go.Scatter(name='Intervalo de Confianza',
                                 x = forecasted.index,
-                                y=forecasted[0]+[round((2.4+i)/24,3) for i in range(len(forecasted))],
+                                y=forecasted[0]+std,
                                 mode='lines',marker=dict(color="#444"),  line=dict(width=0),
                                 fillcolor='rgba(68, 68, 68, 0.3)', fill='tonexty')
 
 low_bound_forecast_chart = go.Scatter(x=forecasted.index,
-                             y=forecasted[0]-[round((4.8+i)/24,3) for i in range(len(forecasted))],
+                             y=forecasted[0]-std,
                              marker=dict(color="#444"), line=dict(width=0), 
                              mode='lines',showlegend=False)
 
@@ -119,13 +128,34 @@ charts = [low_bound_forecast_chart,forecast_load_chart,upper_bound_forecast_char
 date_format='%H:%M %d-%b'
 
 layout = dict(title = "Pronostico de la temperatura : Municipio de Batallas", height=600, width=1000,
-            xaxis = dict(title='Time [H]',tickformat = date_format,
+            xaxis = dict(title='Tiempo [H]',#tickformat = date_format,
                     range=[measured.index[0],forecasted.index[-1]]),
-            yaxis = dict(title='Temperatures [C]'),
+            yaxis = dict(title='Temperatura [°C]'),
             legend=dict(traceorder="reversed"),
             shapes=[dict(type="rect", xref="x", yref="paper",
                          x0=measured.index[-1],y0=0,
                          x1=forecasted.index[-1],
-                         y1=1,fillcolor="LightSalmon",opacity=0.5,
+                         y1=1,fillcolor="LightSalmon",opacity=0.3,
                          layer="below",line_width=0)])
-st.plotly_chart(dict(data=charts, layout=layout))
+fig = go.Figure(data=charts, layout=layout)
+
+f,_=np.where(forecasted.values<=0)
+
+if len(f) == 0:
+    pass
+elif (len(f)==1):
+    
+
+    fig.add_vrect(
+        x0=forecasted.index[f[0]], x1=forecasted.index[f[-1]+1],
+        fillcolor="LightSalmon", opacity=1,
+        layer="below", line_width=0, )
+else:
+    fig.add_vrect(
+        x0=forecasted.index[f[0]], x1=forecasted.index[f[-1]],
+        fillcolor="LightSalmon", opacity=1,
+        layer="below", line_width=0)
+    
+fig.add_hline(y=0, line_dash="dot")
+    
+st.plotly_chart(fig)
